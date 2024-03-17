0b7a3a5187d683f966db2d53dab02bf1b3cf15bf
Guilherme P <guilherme27.gp@gmail.com>
2024-03-16 21:08:32 -0300

    feat: add validation of all requests

--- src/core/validations/params.validation.ts
+++ src/core/validations/params.validation.ts
@@ -1,5 +1,19 @@
 import { z } from 'zod';
 
+const startSchema = z.object({
+    ispb: z.string().regex(/^[0-9]{8}$/),
+})
+
+const insertSchema = z.object({
+    ispb: z.string().regex(/^[0-9]{8}$/),
+    number: z.string().regex(/^[0-9]+$/)
+})
+
+const interationSchema = z.object({
+    ispb: z.string().regex(/^[0-9]+$/),
+    interationId: z.string().uuid(),
+})
+
 const schema = z.object({
     ispb: z.string().min(8).max(8),
     interationId: z.string().uuid(),
@@ -7,5 +21,8 @@ const schema = z.object({
 });
 
 export type Params = z.infer<typeof schema>;
+export type ParamsStart = z.infer<typeof startSchema>;
+export type ParamsInsert = z.infer<typeof insertSchema>;
+export type ParamsInteration = z.infer<typeof interationSchema>;
 
-export default schema;
+export default {schema, interationSchema, insertSchema, startSchema};
