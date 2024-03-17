e4ae2a9beeaa57775d2c2a7b767b62263df7b91f
Guilherme P <guilherme27.gp@gmail.com>
2024-03-16 21:07:54 -0300

    chore: refactor repository atributes and add message type

--- src/core/repositories/message.repository.ts
+++ src/core/repositories/message.repository.ts
@@ -5,11 +5,11 @@ const CreateUser = async (users: User[]) => {
     const data = users.map((user) => {
         return {
             nome: user.nome,
-            cpfcnpj: user.cpfCnpj,
+            cpfCnpj: user.cpfCnpj,
             ispb: user.ispb,
             agencia: user.agencia,
             contaTransacional: user.contaTransacional,
-            tipoconta: user.tipoConta
+            tipoConta: user.tipoConta
         };
     });
 
@@ -50,28 +50,27 @@ const insertData = async (messages: Message[]) => {
 
 const findMessages = async (ispb: string, limit: number = 1) => {
     const messages: Message[] = await db.$queryRaw`
-        select m."endToEndId", m.valor, u1.nome as "nomeRecebedor", u1."cpfcnpj" as "cpfRecebedor", u1."ispb" as "ispbRecebedor",
-        u1."agencia" as "agenciaRecebedor", u1."contaTransacional" as "contaRecebedor", u1."tipoconta" as "tipoRecebedor",
-        u2.nome, u2."cpfcnpj", u2."ispb", u2."agencia", u2."contaTransacional", u2."tipoconta", m."campoLivre", m."txId", m."dataHoraPagamento"
+        select m."endToEndId", m.valor, u1.nome as "nomeRecebedor", u1."cpfCnpj" as "cpfRecebedor", u1."ispb" as "ispbRecebedor",
+        u1."agencia" as "agenciaRecebedor", u1."contaTransacional" as "contaRecebedor", u1."tipoConta" as "tipoRecebedor",
+        u2.nome, u2."cpfCnpj", u2."ispb", u2."agencia", u2."contaTransacional", u2."tipoConta", m."campoLivre", m."txId", m."dataHoraPagamento"
         from "MESSAGE" m, "USER" u1, "USER" u2 
-        where u1.cpfcnpj = m.recebedorid
-        and u2.cpfcnpj = m.pagadorid
+        where u1."cpfCnpj" = m.recebedorid
+        and u2."cpfCnpj" = m.pagadorid
         and u1.ispb = ${ispb}
         order by m."endToEndId"
         limit ${limit}
     `;
-
     return messages;
 };
 
 const findMessagesIterable = async (ispb: string, interationId: string, limit: number = 1) => {
     const messages: Message[] = await db.$queryRaw`
-        select m."endToEndId", m.valor, u1.nome as "nomeRecebedor", u1."cpfcnpj" as "cpfRecebedor", u1."ispb" as "ispbRecebedor",
-        u1."agencia" as "agenciaRecebedor", u1."contaTransacional" as "contaRecebedor", u1."tipoconta" as "tipoRecebedor",
-        u2.nome, u2."cpfcnpj", u2."ispb", u2."agencia", u2."contaTransacional", u2."tipoconta", m."campoLivre", m."txId", m."dataHoraPagamento"
+        select m."endToEndId", m.valor, u1.nome as "nomeRecebedor", u1."cpfCnpj" as "cpfRecebedor", u1."ispb" as "ispbRecebedor",
+        u1."agencia" as "agenciaRecebedor", u1."contaTransacional" as "contaRecebedor", u1."tipoConta" as "tipoRecebedor",
+        u2.nome, u2."cpfCnpj", u2."ispb", u2."agencia", u2."contaTransacional", u2."tipoConta", m."campoLivre", m."txId", m."dataHoraPagamento"
         from "MESSAGE" m, "USER" u1, "USER" u2 
-        where u1.cpfcnpj = m.recebedorid
-        and u2.cpfcnpj = m.pagadorid
+        where u1."cpfCnpj" = m.recebedorid
+        and u2."cpfCnpj" = m.pagadorid
         and u1.ispb = ${ispb}
         and m."endToEndId" > ${interationId}
         order by m."endToEndId"
@@ -81,4 +80,4 @@ const findMessagesIterable = async (ispb: string, interationId: string, limit: n
     return messages;
 };
 
-export default {insertData, findMessages, findMessagesIterable};
+export default { insertData, findMessages, findMessagesIterable };
