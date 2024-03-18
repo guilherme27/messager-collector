a206b3e39444e14932bdc3b73eb494dfb8325a8b
Guilherme P <guilherme27.gp@gmail.com>
2024-03-18 15:25:39 -0300

    chore: change eslint rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'eslint-plugin-import-helpers'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        bracketSameLine: true,
+        arrowParens: 'always',
+        parser: 'typescript',
+        proseWrap: 'always',
+      },
+      'error',
+      {
+        endOfFile: 'auto',
+      },
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [['/^fastify/'], ['module'], ['/^@//'], [('parent', 'sibling', 'index')]],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
+  },
+};
