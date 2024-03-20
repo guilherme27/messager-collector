8073393c852dcbf01fbf09dab8e90d947ce126da
Guilherme P <guilherme27.gp@gmail.com>
2024-03-20 19:09:01 -0300

    fix: fix error that dont find test files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
         exclude: ['**/node_modules/**', '**/dist/**'],
         includeSource: ['src/**/*.spec.ts'],
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
 });
